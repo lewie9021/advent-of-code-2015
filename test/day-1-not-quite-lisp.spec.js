67a88910934fc9f0c75ad172b67c1ff86eecfaac
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-09 21:30:46 +0000

    Day1: Added skeleton specs.

---
+++ test/day-1-not-quite-lisp.spec.js
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import Module, { title } from "../src/day-1-not-quite-lisp";
+
+describe(title, function() {
+
+    xit("should evaluate '(())' and '()()' to 0", function() {
+        
+    });
+
+    xit("should evaluate '(((' and '(()(()(' to 3", function() {
+        
+    });
+
+    xit("should evaluate '))(((((' to 3", function() {
+        
+    });
+
+    xit("should evaluate '())' and '))(' to -1", function() {
+        
+    });
+
+    xit("should evalute ')))' and ')())())' to -3", function() {
+        
+    });
+
+});
