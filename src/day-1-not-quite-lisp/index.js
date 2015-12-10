7c0fcaaabec90b328c64ca66ecc75fabe51b639e
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-10 01:52:20 +0000

    Completed part 2 of the challenge. - Refactored most of the code from part 1 to share functionality in this section. - Spoiler alert: the answer is 1771.

--- src/day-1-not-quite-lisp/index.js
+++ src/day-1-not-quite-lisp/index.js
@@ -4,27 +4,41 @@ import _ from "lodash";
 
 export const title = "Day 1: Not Quite Lisp";
 
-function countValue(x) {   
-    return _.curry((arr) => _.filter(arr, (value) => value == x));
+const directions = {
+    "(": 1,
+    ")": -1
+};
+
+function get(x) {
+    return _.curry((obj) => obj[x]);
+}
+
+function followInstructions(instructions) {
+    let base = {
+        position: null,
+        floor: 0
+    };
+    
+    return _.reduce(instructions, (details, instruction, index) => {
+        const floor = details.floor + directions[instruction];
+        const pos = (!details.position && floor < 0) ? index + 1 : details.position;
+        
+        return {floor, position: pos};
+    }, base);
 }
 
 export function getFloor(instructions) {
-    const get = (x) => _.curry((chars) => chars[x]);
-    const count = (character) =>  _.compose(get("length"), countValue(character), _.map)(instructions);
-
-    const up = count("(");
-    const down = count(")") * -1;
-    
-    return up + down;
+    return _.compose(get("floor"), followInstructions, _.map)(instructions);
 }
 
-export function atBasement(instructions) {
-    
+export function getBasement(instructions) {
+    return _.compose(get("position"), followInstructions, _.map)(instructions);
 }
 
 export function run() {
     const inputPath = Path.join(__dirname, "input.txt");
-    const instructions = FS.readFileSync(inputPath, "utf-8");
+    const instructions = FS.readFileSync(inputPath, "utf-8").trim();
     
-    console.log("To what floor do the instructions take Santa?", FloorCalculator(instructions));
+    console.log("To what floor do the instructions take Santa?", getFloor(instructions));
+    console.log("What is the position of the character that causes Santa to first enter the basement?", getBasement(instructions));
 }
