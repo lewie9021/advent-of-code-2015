560336b8210b7775f2f3e715193c6b7e3a0b7c3e
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-10 23:55:13 +0000

    Initial attempt at solving the challenge. The specs are passing...

--- src/day-2/index.js
+++ src/day-2/index.js
@@ -1,8 +1,24 @@
 import FS from "fs";
 import Path from "path";
-import _ from "lodash";
+import _ from "lodash-fp";
 
 export const title = "Day 2: I Was Told There Would Be No Math";
+
+function split(delimiter) {
+    return _.curry((str) => str.split(delimiter));
+}
+
+export function getSquareFootage(measurements) {
+    const parse = _.compose(_.map(_.parseInt(10)), split("x"));
+    const [length, width, height] = parse(measurements);
+
+    // Do some math...
+    const surfaceArea = (2 * length * width) + (2 * width * height) + (2 * height * length);
+    const smallestSide = Math.min(width * height, length * height, width * length);
+
+    // Total surface area + the area of the smallest side.
+    return surfaceArea + smallestSide;
+}
 
 export function run() {
     const inputPath = Path.join(__dirname, "input.txt");
--- src/day-2/index.spec.js
+++ src/day-2/index.spec.js
@@ -1,16 +1,16 @@
 import { expect } from "chai";
-import { title } from "./";
+import { title, getSquareFootage } from "./";
 
-describe(title, function() {
+describe.only(title, function() {
 
     describe("Part 1:", function() {
     
-        xit("should evaluate '2x3x4' as 58 square feet", function() {
-            
+        it("should evaluate '2x3x4' as 58 square feet", function() {
+            expect(getSquareFootage("2x3x4")).to.eq(58);
         });
 
-        xit("should evaluate '1x1x10' as 43 square feet", function() {
-            
+        it("should evaluate '1x1x10' as 43 square feet", function() {
+            expect(getSquareFootage("1x1x10")).to.eq(43);
         });
 
     });
