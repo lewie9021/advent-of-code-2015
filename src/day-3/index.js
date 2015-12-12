4f26483b613c1b2af2eb0e76aa6c8a4db5154fe0
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-12 17:23:15 +0000

    Completed part 1 of day 3. - Spoiler alert: the answer is 2592.

--- src/day-3/index.js
+++ src/day-3/index.js
@@ -8,8 +8,31 @@ function split(delimiter) {
     return _.curry((str) => str.split(delimiter));
 }
 
+const directions = {
+    ">": [1, 0],
+    "<": [-1, 0],
+    "v": [0, 1],
+    "^": [0, -1]
+};
+
 export function getDeliveredHouses(instructions) {
+    const navigate = _.reduce((tracking, instruction) => {
+        const [x, y] = directions[instruction];
+        const xx = tracking.x + x;
+        const yy = tracking.y + y;
+        
+        return {
+            houses: Object.assign({}, tracking.houses, {
+                [`${xx}-${yy}`]: null
+            }),
+            x: xx,
+            y: yy
+        };
+    }, {houses: {"0-0": null}, x: 0, y: 0});
+    const total = _.compose(_.get("length"), Object.keys, _.get("houses"));
+    const deliver = _.compose(total, navigate, split(""));
     
+    return deliver(instructions);
 }
 
 export function run() {
