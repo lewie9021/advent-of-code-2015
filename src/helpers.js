b169dc98309309810c90587e4a2fecb8d766f358
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-13 16:47:54 +0000

    Added a workaround for recursion called 'doWhile'. - Made the split helper fully curried.

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,7 @@
 import __ from "lodash";
 import _ from "lodash-fp";
 
-export function split(delimiter) {
-    return _.curry((str) => str.split(delimiter));
-}
+export const split = _.curry((delimiter, str) => str.split(delimiter));
 
 export function log(x) {
     console.log(x);
@@ -12,3 +10,12 @@ export function log(x) {
 }
 
 export const groupBy = _.curryRight(__.groupBy, 2);
+
+export const doWhile = _.curry((f, g, x) => {
+    let y = f(x);
+    
+    while (g(y))
+        y = f(y);
+
+    return y;
+});
