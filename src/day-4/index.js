2b9bb1ff1c54f5918050ba62bbe61a9c6c391449
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-13 11:51:32 +0000

    First attempt at part one using recursion. - JavaScript doesn't have tail call optimization yet :( - Added a md5 function to make hash creation easier.

--- src/day-4/index.js
+++ src/day-4/index.js
@@ -1,11 +1,29 @@
 import FS from "fs";
 import Path from "path";
+import Crypto from "crypto";
 import _ from "lodash-fp";
 
 export const title = "Day 4: The Ideal Stocking Stuffer";
 
+function md5(value) {
+    return Crypto
+        .createHash("md5")
+        .update(value)
+        .digest("hex");
+}
+
+// Requires tail-end recursion. It exceeds the max call stack way too soon.
+function findHashIndex(key, i) {
+    const hash = md5(`${key}${i}`);
+
+    if (hash.indexOf("00000") == 0)
+        return i;
+
+    return findHashIndex(key, i + 1);
+}
+
 export function getHashWithFiveZeros(key) {
-    
+    // return findHashIndex(key, 0);
 }
 
 export function run() {
