4fc671da3faadd7d1fa02b0ffe4f87e21bedcfb9
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-13 18:27:33 +0000

    Composed the first rule.

--- src/day-5/index.js
+++ src/day-5/index.js
@@ -1,10 +1,20 @@
 import FS from "fs";
 import Path from "path";
 import _ from "lodash-fp";
+import { split } from "../helpers";
 
 export const title = "Day 5: Doesn't He Have Intern-Elves For This?";
 
 export function isNiceString(str) {
+    // Contains at least 3 vowels.
+    const vowels = ["a", "e", "i", "o", "u"];
+    const threeVowels = _.compose(_.gte(3), _.get("length"), _.filter((x) => _.includes(x, vowels)), split(""))(str);
+    
+    // Contains at least one letter that appears twice.
+    
+    
+    // Doesn't contain 'ab', 'cd', 'pq', or 'xy'.
+
     
 }
 
