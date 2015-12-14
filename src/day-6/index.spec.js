6138ddd2f04885613605364b4529b60c505003c4
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-14 20:30:56 +0000

    Modified the wording of the specs.

--- src/day-6/index.spec.js
+++ src/day-6/index.spec.js
@@ -5,15 +5,15 @@ describe(title, function() {
 
     describe("Part 1:", function() {
 
-        it("should evaluate 'turn on 0,0 through 999,999' to turn on every light", function() {
+        it("should evaluate 'turn on 0,0 through 999,999' as turn on every light", function() {
             
         });
 
-        it("should evaluate 'toggle 0,0 through 999,0' to toggle the first 1000 lights", function() {
+        it("should evaluate 'toggle 0,0 through 999,0' as toggle the first 1000 lights", function() {
             
         });
 
-        it("should evaluate 'turn off 499,499 through 500,500' to turn off the middle four lights.", function() {
+        it("should evaluate 'turn off 499,499 through 500,500' as turn off the middle four lights.", function() {
             
         });
         
