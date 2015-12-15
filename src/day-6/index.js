fbc1c4aa203540964cb1cba8f0d38f64b00967b9
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-15 19:39:04 +0000

    Added a parse function. A grid is now passed to configureLighting. - This is so that a series of instructions be executed on a single grid. Having the grid creation within the function would remove this ability.

--- src/day-6/index.js
+++ src/day-6/index.js
@@ -1,19 +1,30 @@
 import FS from "fs";
 import Path from "path";
 import _ from "lodash-fp";
-import { fill } from "../helpers";
+import { split, fill } from "../helpers";
 
 export const title = "Day 6: Probably a Fire Hazard";
 
-function createGrid(width, height, value = 0) {
-    let columns = fill(value, Array(width));
-    let rows = () => fill(value, Array(height));
-    
-    return _.map(rows, columns);
+export function createGrid(width, height, value = 0) {
+    let rows = fill(value, Array(height));
+    const columns = () => fill(value, Array(width));
+
+    return _.map(columns, rows);
 }
 
-export function configureLighting(instruction) {
+function parse(instruction) {
+    const toIntegers = _.compose(_.map(_.parseInt(10)), split(","));
+    const [mode, pointA, pointB] = instruction.match(/(on|off|toggle)|(\d+),(\d+)/g);
     
+    return {
+        mode,
+        pointA: toIntegers(pointA),
+        pointB: toIntegers(pointB)
+    };
+}
+
+export function configureLighting(grid, instructions) {
+    const {mode, pointA, pointB} = parse(instructions);
 }
 
 export function run() {
