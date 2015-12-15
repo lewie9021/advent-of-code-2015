e2ba7a5f2401745e9546ff19eb9e377e4b2962c1
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-15 18:42:12 +0000

    Added a function to aid grid creation. - Defaults to all lights off.

--- src/day-6/index.js
+++ src/day-6/index.js
@@ -1,8 +1,20 @@
 import FS from "fs";
 import Path from "path";
 import _ from "lodash-fp";
+import { fill } from "../helpers";
 
 export const title = "Day 6: Probably a Fire Hazard";
+
+function createGrid(width, height, value = 0) {
+    let columns = fill(value, Array(width));
+    let rows = () => fill(value, Array(height));
+    
+    return _.map(rows, columns);
+}
+
+export function configureLighting(instruction) {
+    
+}
 
 export function run() {
     const inputPath = Path.join(__dirname, "input.txt");
