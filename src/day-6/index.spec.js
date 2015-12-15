958c2ceb21c54ca8d26108321f9d7ce96a52667c
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-15 19:34:08 +0000

    Added test code for the first expectation.

--- src/day-6/index.spec.js
+++ src/day-6/index.spec.js
@@ -1,12 +1,22 @@
+import _ from "lodash-fp";
 import { expect } from "chai";
-import { title } from "./";
+import { title, createGrid, configureLighting } from "./";
 
 describe(title, function() {
 
     describe("Part 1:", function() {
 
+        beforeEach(function() {
+            this.grid = createGrid(1000, 1000);
+        });
+        
         it("should evaluate 'turn on 0,0 through 999,999' as turn on every light", function() {
+            const result = configureLighting(this.grid, "turn on 0,0 through 999,999");
+            const count = _.reduce((count, row) => {
+                return count + _.sum(row);
+            }, 0, result);
             
+            expect(count).to.eq(1000 * 1000);
         });
 
         it("should evaluate 'toggle 0,0 through 999,0' as toggle the first 1000 lights", function() {
