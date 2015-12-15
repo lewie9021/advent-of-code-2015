ad842056d6afd523d3f7930bdd0a74830d256e3d
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-15 18:41:19 +0000

    Added a better _.fill implementation -.-

--- src/helpers.js
+++ src/helpers.js
@@ -11,6 +11,9 @@ export function log(x) {
 
 export const groupBy = _.curryRight(__.groupBy, 2);
 
+// A less annoying _.fill function.
+export const fill = _.curry((x, arr) => _.map(() => x, arr));
+
 export const doWhile = _.curry((f, g, x) => {
     let y = f(x);
     
