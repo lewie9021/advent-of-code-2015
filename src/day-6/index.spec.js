688ee952012ecb59c5d304c4104a1726c12e131d
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-15 20:50:08 +0000

    Made the tests pass for the third expectation.

--- src/day-6/index.spec.js
+++ src/day-6/index.spec.js
@@ -3,25 +3,20 @@ import { expect } from "chai";
 import { title, createGrid, configureLighting } from "./";
 
 describe(title, function() {
+    const total = _.compose(_.sum, _.map(_.sum));
 
     describe("Part 1:", function() {
 
-        beforeEach(function() {
-            this.grid = createGrid(1000, 1000);
-        });
-        
         it("should evaluate 'turn on 0,0 through 999,999' as turn on every light", function() {
-            const result = configureLighting(this.grid, "turn on 0,0 through 999,999");
-            const count = _.reduce((count, row) => {
-                return count + _.sum(row);
-            }, 0, result);
+            const grid = createGrid(1000, 1000);
+            const result = configureLighting(grid, "turn on 0,0 through 999,999");
             
-            expect(count).to.eq(1000 * 1000);
+            expect(total(result)).to.eq(1000 * 1000);
         });
 
         it("should evaluate 'toggle 0,0 through 999,0' as toggle the first 1000 lights", function() {
-            const total = _.compose(_.sum, _.map(_.sum));
-            const result = configureLighting(this.grid, "toggle 0,0 through 999,0");
+            const grid = createGrid(1000, 1000);
+            const result = configureLighting(grid, "toggle 0,0 through 999,0");
 
             // Check the first row of lights are on.
             expect(_.sum(result[0])).to.eq(1000);
@@ -34,7 +29,17 @@ describe(title, function() {
         });
 
         it("should evaluate 'turn off 499,499 through 500,500' as turn off the middle four lights.", function() {
+            const grid = createGrid(1000, 1000, true);
+            const result = configureLighting(grid, "turn off 499,499 through 500,500");
             
+            // Make sure the correct lights are turned off.
+            expect(result[499][499]).to.eq(false);
+            expect(result[499][500]).to.eq(false);
+            expect(result[500][499]).to.eq(false);
+            expect(result[500][500]).to.eq(false);
+            
+            // Make sure all but 4 lights are on.
+            expect(total(result)).to.eq((1000 * 1000) - 4);
         });
         
     });
