0b8ad6dd30040971fe9d42cb59c0795d611b9121
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-16 20:44:57 +0000

    Got the second test passing

--- src/day-7/index.js
+++ src/day-7/index.js
@@ -5,6 +5,23 @@ import { split } from "../helpers";
 
 export const title = "Day 7: Some Assembly Required";
 
+const operators = {
+    "AND": (x, y) => x & y,
+    "OR": (x, y) => x | y,
+    "LSHIFT": (x, y) => x << y,
+    "RSHIFT": (x, y) => x >> y,
+    "NOT": (x) => ~x
+};
+
+function lookupValues(scope, values) {
+    return values
+        .match(/([A-Z]+)|([0-9]+)/gi)
+        .map((x) => {
+            const value = parseInt(x, 10);
+            return (isNaN(value)) ? scope[x] : value;
+        });
+}
+
 export function execute(baseScope, instruction) {
     const scope = _.clone(baseScope);
     const [operation, identifier] = _.map(_.trim, split("->", instruction));
@@ -16,6 +33,17 @@ export function execute(baseScope, instruction) {
 
         return scope;
     }
+
+    // Get the operator function.
+    const pattern = new RegExp(`(${Object.keys(operators).join("|")})`, "g");
+    const [name] = operation.match(pattern);
+    const operator = operators[name];
+    
+    // Parse the values so we are working with constants.
+    const values = lookupValues(scope, operation.replace(name, ""));
+
+    // Pass the values to the operator and create / update the value in scope.
+    scope[identifier] = operator(...values);
 
     return scope;
 }
