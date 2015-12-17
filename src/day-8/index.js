e85ed1fe73295fa95cc12329ff908446e22d3e22
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-17 20:13:52 +0000

    Added boilerplate code for day 8.

---
+++ src/day-8/index.js
@@ -0,0 +1,20 @@
+import FS from "fs";
+import Path from "path";
+import _ from "lodash-fp";
+
+export const title = "Day 8: Matchsticks";
+
+export function charactersInCode() {
+    
+}
+
+export function charactersInMemory() {
+    
+}
+
+export function run() {
+    const inputPath = Path.join(__dirname, "input.txt");
+    const input = FS.readFileSync(inputPath, "utf-8").trim();
+    
+    console.log("what is the number of characters of code for string literals minus the number of characters in memory for the values of the strings in total for the entire file?");
+}
---
+++ src/day-8/index.spec.js
@@ -0,0 +1,50 @@
+import { expect } from "chai";
+import { title, charactersInCode, charactersInMemory } from "./";
+
+describe(title, function() {
+
+    describe("Part 1:", function() {
+
+        describe("charactersInCode", function() {
+        
+            it(`should evaluate '""' as 2 characters`, function() {
+                
+            });
+
+            it(`should evaluate '"abc"' as 5 characters`, function() {
+                
+            });
+
+            it(`should evaluate '"aaa\\"aaa"' as 10 characters`, function() {
+                
+            });
+
+            it(`should evaluate '"\\x27"' as 6 characters`, function() {
+                
+            });
+
+        });
+
+        describe("charactersInMemory", function() {
+        
+            it(`should evaluate '""' as 0 characters`, function() {
+                
+            });
+
+            it(`should evaluate '"abc"' as 3 characters`, function() {
+                
+            });
+
+            it(`should evaluate '"aaa\\"aaa"' as 7 characters`, function() {
+                
+            });
+
+            it(`should evaluate '"\\x27"' as 1 character`, function() {
+                
+            });
+
+        });
+
+    });
+
+});
