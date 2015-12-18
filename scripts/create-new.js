773fc69e2fa03f094bb61ba2353cf7f350916600
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-18 13:57:55 +0000

    Fixed a bug with the creation script, caused by moving directories.

--- scripts/create-new.js
+++ scripts/create-new.js
@@ -2,9 +2,10 @@ var Path = require("path");
 var FS = require("fs-extra");
         
 var name = process.argv[2];
-var output = Path.join(__dirname, "src", name);
+var root = Path.join(__dirname, "..");
+var output = Path.join(root, "src", name);
 
 console.log("Creating", output);
-FS.copySync(Path.join(__dirname, "boilerplate"), output);
+FS.copySync(Path.join(root, "boilerplate"), output);
 
 console.log("Operation successful.");
