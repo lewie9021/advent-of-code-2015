f0c4a965857ce0eb6e169281d4d9dfee74d2ea47
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-18 15:52:59 +0000

    Added a couple of functions: getRoutes and getLocations. - getRoutes: converts the initial input into an array of objects containing the keys 'from', 'to', and 'distance'. - getLocations: returns a map of unique locations using the array from getRoutes.

--- src/day-9/index.js
+++ src/day-9/index.js
@@ -4,6 +4,38 @@ import _ from "lodash-fp";
 
 export const title = "Day 9: All in a Single Night";
 
+export function getLocations(routes) {
+    const locations = _.compose(_.unique, _.map(_.get("from")));
+
+    return _.reduce((map, route, index) => {
+        map[route] = index;
+        
+        return map;
+    }, {}, locations(routes));
+}
+
+export function getRoutes(distances) {
+    const route = (string) => {
+        const values = string
+                  .replace("to", "")
+                  .match(/(\w+|\d+)/g);
+        const distance = parseInt(values[2]);
+
+        return [
+            {from: values[0], to: values[1], distance},
+            {from: values[1], to: values[0], distance}
+        ];
+    };
+    const routes = _.compose(_.flatten, _.map(route));
+
+    return routes(distances);
+}
+
+export function shortestDistance(distances) {
+    const routes = getRoutes(distances);
+    const locations = getLocations(routes);
+}
+
 export function run() {
     const inputPath = Path.join(__dirname, "input.txt");
     const input = FS.readFileSync(inputPath, "utf-8").trim();
--- src/day-9/index.spec.js
+++ src/day-9/index.spec.js
@@ -1,11 +1,46 @@
 import { expect } from "chai";
-import { title } from "./";
+import { title, getRoutes, getLocations, shortestDistance } from "./";
 
 describe(title, function() {
 
     describe("Part 1:", function() {
 
-        it("should work", function() {
+        beforeEach(function() {
+            this.distances = [
+                "London to Dublin = 464",
+                "London to Belfast = 518",
+                "Dublin to Belfast = 141"
+            ];
+        });
+        
+        describe("getRoutes", function() {
+
+            it("should return an array of routes", function() {
+                const routes = getRoutes(this.distances);
+                
+                expect(Array.isArray(routes)).to.eq(true);
+            });
+
+            it("should duplicate the number of routes (there and back)", function() {
+                const routes = getRoutes(this.distances);
+
+                expect(routes.length).to.eq(this.distances.length * 2);
+            });
+            
+        });
+
+        describe("getLocations", function() {
+
+            beforeEach(function() {
+                this.routes = getRoutes(this.distances);
+            });
+            
+            it("should return a map of unique locations", function() {
+                const locations = getLocations(this.routes);
+                
+                expect(Object.keys(locations).length).to.eq(3);
+                expect(locations).to.have.keys("London", "Dublin", "Belfast");
+            });
             
         });
 
