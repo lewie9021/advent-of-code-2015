d9be2487e9f011cdc38c666ba1cc896c252dbae4
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-19 16:31:20 +0000

    Added boilerplate code for day 10.

---
+++ src/day-10/index.js
@@ -0,0 +1,16 @@
+import FS from "fs";
+import Path from "path";
+import _ from "lodash-fp";
+
+export const title = "Day 10: Elves Look, Elves Say";
+
+export function lookSay(sequence) {
+    
+}
+
+export function run() {
+    const inputPath = Path.join(__dirname, "input.txt");
+    const input = FS.readFileSync(inputPath, "utf-8").trim();
+    
+    console.log("What is the length of the result?");
+}
