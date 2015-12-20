d478bd74e9ceac82ca064f6fa14dc6bc79255cf1
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-20 22:07:11 +0000

    Fixed some typos with the test spec. - Completed missed the words 'gain' and 'lose'. Oops...

--- src/day-13/index.spec.js
+++ src/day-13/index.spec.js
@@ -8,12 +8,12 @@ describe(title, function() {
         it("should return 330 as the total optimal happiness for the given example", function() {
             const input = [
                 {guest: "Alice", neighbour: "Bob", value: 54},
-                {guest: "Alice", neighbour: "Carol", value: 79},
-                {guest: "Alice", neighbour: "David", value: 2},
+                {guest: "Alice", neighbour: "Carol", value: -79},
+                {guest: "Alice", neighbour: "David", value: -2},
                 {guest: "Bob", neighbour: "Alice", value: 83},
-                {guest: "Bob", neighbour: "Carol", value: 7},
-                {guest: "Bob", neighbour: "David", value: 63},
-                {guest: "Carol", neighbour: "Alice", value: 62},
+                {guest: "Bob", neighbour: "Carol", value: -7},
+                {guest: "Bob", neighbour: "David", value: -63},
+                {guest: "Carol", neighbour: "Alice", value: -62},
                 {guest: "Carol", neighbour: "Bob", value: 60},
                 {guest: "Carol", neighbour: "David", value: 55},
                 {guest: "David", neighbour: "Alice", value: 46},
