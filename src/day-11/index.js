7f443769266745561154c481fccaf863e0d8f9a7
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-20 11:07:56 +0000

    Added boilerplate code for day 11.

---
+++ src/day-11/index.js
@@ -0,0 +1,12 @@
+import FS from "fs";
+import Path from "path";
+import _ from "lodash-fp";
+
+export const title = "Day 11: Corporate Policy";
+
+export function run() {
+    const inputPath = Path.join(__dirname, "input.txt");
+    const input = FS.readFileSync(inputPath, "utf-8").trim();
+    
+    console.log("What should Santa's next password be?");
+}
---
+++ src/day-11/index.spec.js
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+import { title } from "./";
+
+describe(title, function() {
+
+    describe("Part 1:", function() {
+
+        it("should evaluate 'hijklmmn' as invalid", function() {
+            
+        });
+
+        it("should evaluate 'abbceffg' as invalid", function() {
+            
+        });
+
+        it("should evaluate 'abbcegjk' as invalid", function() {
+            
+        });
+
+        it("should return the next password after 'abcdefgh' as 'abcdffaa'", function() {
+            
+        });
+
+        it("should return the next password after 'ghijklmn' as 'ghjaabcc'", function() {
+            
+        });
+
+    });
+
+});
