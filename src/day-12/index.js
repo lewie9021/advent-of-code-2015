4abf1bcfb18d48428e5b9077e05b136ac11bca7e
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-20 19:25:54 +0000

    Added a reduceDeep function. All specs pass. - TODO: Added specs to ensure only numbers are added. - reduceDeep is similar to the normal reduce, only this visits deeply nested values in the collection given.

--- src/day-12/index.js
+++ src/day-12/index.js
@@ -4,6 +4,31 @@ import _ from "lodash-fp";
 
 export const title = "Day 12: JSAbacusFramework.io";
 
+function visit(node) {
+    if (_.isArray(node))
+        return _.map((x) => visit(x), node);
+
+    if (_.isObject(node))
+        return _.map((k) => visit(node[k]), Object.keys(node));
+    
+    return node;
+}
+
+function reduceDeep(collection, func, start) {
+    const values = _.flattenDeep(visit(collection));
+    let value = start;
+
+    for (let i = 0; i < values.length; i += 1) {
+        value = func(value, values[i], i, values);
+    }
+    
+    return value;
+}
+
+export function total(collection) {
+    return reduceDeep(collection, _.add, 0);
+}
+
 export function run() {
     const inputPath = Path.join(__dirname, "input.json");
     const input = JSON.parse(FS.readFileSync(inputPath, "utf-8"));
--- src/day-12/index.spec.js
+++ src/day-12/index.spec.js
@@ -1,20 +1,32 @@
 import { expect } from "chai";
-import { title } from "./";
+import { title, total } from "./";
 
 describe(title, function() {
 
     describe("Part 1:", function() {
 
-        it("should evaluate '[]' and '{}' as 0", function() {
-            
+        it(`should evaluate '[]' and '{}' as 0`, function() {
+            expect(total([])).to.eq(0);
+            expect(total({})).to.eq(0);
         });
 
-        it("should evaluate '[1,2,3]' and '{\"a\":2,\"b\":4}' as 6", function() {
-            
+        it(`should evaluate '[1,2,3]' and '{a: 2, b: 4}' as 6`, function() {
+            expect(total([1,2,3])).to.eq(6);
+            expect(total({a: 2, b: 4})).to.eq(6);
         });
 
-        it("should evaluate '[[[3]]]' and '{\"a\":{\"b\":4},\"c\":-1}' as 3", function() {
-            
+        it("should evaluate '[[[3]]]' and '{a: {b: 4}, c: -1}' as 3", function() {
+            expect(total([[[3]]])).to.eq(3);
+            expect(total({a: {b: 4}, c: -1})).to.eq(3);
+        });
+
+        it(`should evaluate '[-1 ,{a: 1}]' and '{a: [-1, 1]}' as 0`, function() {
+            expect(total({a: [-1, 1]})).to.eq(0);
+            expect(total([-1 ,{a: 1}])).to.eq(0);
+        });
+
+        it(`should evaluate '{a: {b: [1, 2, 3], c: {d: [3, 2, 1]}}}' as 12`, function() {
+            expect(total({a: {b: [1, 2, 3], c: {d: [3, 2, 1]}}})).to.eq(12);
         });
 
     });
