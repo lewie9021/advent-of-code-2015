977a3d4ffa131f7b848d65e455ce844054a71a42
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-26 22:24:12 +0000

    Completed the implementation of getCombinations.

--- src/day-17/index.js
+++ src/day-17/index.js
@@ -4,6 +4,22 @@ import _ from "lodash-fp";
 
 export const title = "Day 17: No Such Thing as Too Much";
 
+export function getCombinations(list, target, partial = [], result = []) {
+    const partialTotal = _.sum(partial);
+
+    if (_.isEqual(partialTotal, target))
+        result.push(partial);
+
+    _.map((index) => {
+        const value = list[index];
+        const rest = list.slice(index + 1);
+
+        getCombinations(rest, target, partial.concat(value), result);
+    }, _.range(0, list.length));
+
+    return result;
+};
+
 export function run() {
     const inputPath = Path.join(__dirname, "input.txt");
     const input = FS.readFileSync(inputPath, "utf-8").trim().split("\n");
