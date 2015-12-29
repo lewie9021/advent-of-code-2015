e57961d1e4b449d447e283439321ab4ced8825d4
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-29 14:46:29 +0000

    Completed part 1. Rewrote both functions due to hanging during calc. - Spoiler alert: the answer is 665280.

--- src/day-20/index.js
+++ src/day-20/index.js
@@ -1,43 +1,48 @@
 import FS from "fs";
 import Path from "path";
 import _ from "lodash-fp";
-import { multiply } from "../helpers";
+import { log } from "../helpers";
 
 export const title = "Day 20: Infinite Elves and Infinite Houses";
 
-export function getPresentCount(houseNumber) {
-    return _.compose(
-        _.sum,
-        _.map(multiply(10)),
-        _.filter((x) => houseNumber % x == 0),
-        _.range(1)
-    )(houseNumber + 1);
+export function getPresentCounts(target) {
+    const max = target / 10;
+    
+    return _.reduce((houses, multiple) => {
+        const multiples = _.map((index) => {
+            return multiple + (index * multiple);
+        }, _.range(0, Math.floor(max / multiple)));
+        
+        _.forEach((x) => {
+            houses[x] = (houses[x] || 0) + (multiple * 10);
+        }, multiples);
+
+        return houses;
+    }, {}, _.range(1, max + 1));
 }
 
 export function getLowestHouseNumber(target) {
-    const firstMatch = (target, index = 0) => {
-        if (getPresentCount(index) >= target)
-            return index;
-
-        return firstMatch(target, index + 1);
-    };
-    const index = firstMatch(target);
+    const counts = getPresentCounts(target);
 
     return _.compose(
         _.get("house"),
         _.first,
-        _.sortByAll(["presents", "house"]),
+        _.sortByAll(["house", "presents"]),
         _.filter(({presents}) => presents >= target),
-        _.map((count) => ({
-            house: index + count,
-            presents: getPresentCount(index + count)
-        }))
-    )(_.range(0, 3));
+        _.map((index) => {
+            const key = parseInt(index, 10);
+            
+            return  {
+                house: key,
+                presents: counts[key]
+            };
+        })
+    )(Object.keys(counts));
 }
 
 export function run() {
     const inputPath = Path.join(__dirname, "input.txt");
-    const input = FS.readFileSync(inputPath, "utf-8").trim();
+    const input = _.parseInt(10, FS.readFileSync(inputPath, "utf-8").trim());
     
-    console.log("What is the lowest house number of the house to get at least as many presents as the number in your puzzle input?");
+    console.log("What is the lowest house number of the house to get at least as many presents as the number in your puzzle input?", getLowestHouseNumber(input));
 }
--- src/day-20/index.spec.js
+++ src/day-20/index.spec.js
@@ -1,27 +1,59 @@
 import { expect } from "chai";
-import { title, getPresentCount, getLowestHouseNumber } from "./";
+import { title, getPresentCounts, getLowestHouseNumber } from "./";
 import _ from "lodash-fp";
 
 describe(title, function() {
 
     describe("Part 1:", function() {
 
-        describe("getPresentCount", function() {
+        describe("getPresentCounts", function() {
             const counts = {
-                1: 10,
-                2: 30,
-                3: 40,
-                4: 70,
-                5: 60,
-                6: 120,
-                7: 80,
-                8: 150,
-                9: 130
+                20: {
+                    1: 10,
+                    2: 30
+                },
+                60: {
+                    1: 10,
+                    2: 30,
+                    3: 40,
+                    4: 70,
+                    5: 60,
+                    6: 120
+                },
+                100: {
+                    1: 10,
+                    2: 30,
+                    3: 40,
+                    4: 70,
+                    5: 60,
+                    6: 120,
+                    7: 80,
+                    8: 150,
+                    9: 130,
+                    10: 180
+                },
+                150: {
+                    1: 10,
+                    2: 30,
+                    3: 40,
+                    4: 70,
+                    5: 60,
+                    6: 120,
+                    7: 80,
+                    8: 150,
+                    9: 130,
+                    10: 180,
+                    11: 120,
+                    12: 280,
+                    13: 140,
+                    14: 240,
+                    15: 240
+                }
             };
-
+            
             _.forEach((key) => {
-                it(`should return ${counts[key]}, given ${key}`, function() {
-                    expect(getPresentCount(key)).to.eq(counts[key]);
+                it(`should return a map of present counts, given ${key}`, function() {
+                    expect(getPresentCounts(key)).to.eql(counts[key]);
                 });
             }, Object.keys(counts));
         });
@@ -29,7 +61,7 @@ describe(title, function() {
         describe("getLowestHouseNumber", function() {
             const counts = {
                 20: 2,
-                60: 5,
+                60: 4,
                 100: 6,
                 150: 8
             };
