e6e9035db8f34fee9830ef724b173a91294738e4
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-30 20:30:09 +0000

    Completed the implementation of parse.

--- src/day-21/index.js
+++ src/day-21/index.js
@@ -1,8 +1,23 @@
 import FS from "fs";
 import Path from "path";
 import _ from "lodash-fp";
+import { split } from "../helpers";
 
 export const title = "Day 21: RPG Simulator 20XX";
+
+export function parse(input) {
+    const [health, damage, armor] = _.compose(
+        _.map(_.parseInt(10)),
+        _.map((line) => line.match(/\d+/)),
+        split("\n")
+    )(input);
+
+    return {
+        health,
+        damage,
+        armor
+    };
+}
 
 export function run() {
     const inputPath = Path.join(__dirname, "input.txt");
