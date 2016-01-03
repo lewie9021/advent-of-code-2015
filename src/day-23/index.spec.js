9035e9f75d5b64ef0b7f97c86ce4e7eed056b7f9
Lewis Barnes <lb.barnes@live.co.uk>
2016-01-03 21:51:14 +0000

    Added specs for a parse function.

--- src/day-23/index.spec.js
+++ src/day-23/index.spec.js
@@ -1,11 +1,53 @@
 import { expect } from "chai";
-import { title } from "./";
+import { title, parse } from "./";
 
 describe(title, function() {
 
     describe("Part 1:", function() {
 
-        it("should work", function() {
+        describe("parse", function() {
+
+            it("should return an array of instructions", function() {
+                const input = [
+                    "inc a",
+                    "hlf a"
+                ].join("\n");
+                
+                expect(parse(input)).to.be.an("array");
+            });
+
+            it("should return objects with properties: 'operator' and 'operands'", function() {
+                const input = [
+                    "inc a",
+                    "hlf a"
+                ].join("\n");
+                const result = parse(input);
+
+                expect(result.length).to.eq(2);
+
+                result.forEach((instruction) => {
+                    expect(instruction.operator).to.be.a("string");
+                    expect(instruction.operands).to.be.an("array");
+                });
+            });
+
+            it("should correctly evaluate an instruction with one operator and operand", function() {
+                const result = parse("inc a");
+
+                expect(result).to.eql([{
+                    operator: "inc",
+                    operands: ["a"]
+                }]);
+            });
+
+            it("should correctly evaluate an instruction with one operator and multiple operands", function() {
+                const result = parse("jio a, +18");
+
+                expect(result).to.eql([{
+                    operator: "jio",
+                    operands: ["a", "+18"]
+                }]);
+            });
             
         });
 
