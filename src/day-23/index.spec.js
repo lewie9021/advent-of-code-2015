c07e3a5d080ec0b1960e6eb8a687a1c8230bd6ce
Lewis Barnes <lb.barnes@live.co.uk>
2016-01-03 22:24:40 +0000

    Added specs for an execute function.

--- src/day-23/index.spec.js
+++ src/day-23/index.spec.js
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { title, parse } from "./";
+import { title, parse, execute } from "./";
 
 describe(title, function() {
 
@@ -51,6 +51,118 @@ describe(title, function() {
             
         });
 
+        describe("execute", function() {
+
+            it("should support the inc (increment) operator", function() {
+                const input = [
+                    "inc a",
+                    "inc b"
+                ].join("\n");
+                const instructions = parse(input);
+                const results = execute(instructions);
+
+                expect(results).to.eql({a: 1, b: 1});
+            });
+            
+            it("should support the hlf (half) operator", function() {
+                const input = [
+                    "inc a",
+                    "inc a",
+                    "inc a",
+                    "inc a",
+                    "hlf a",
+                ].join("\n");
+                const instructions = parse(input);
+                const results = execute(instructions);
+
+                expect(results).to.eql({a: 2, b: 0});
+            });
+
+            it("should floor floats as a result of the hlf operator", function() {
+                const input = [
+                    "inc a",
+                    "inc a",
+                    "inc a",
+                    "inc b",
+                    "hlf a",
+                    "hlf b"
+                ].join("\n");
+                const instructions = parse(input);
+                const results = execute(instructions);
+
+                expect(results).to.eql({a: 1, b: 0});
+            });
+
+            it("should support the tpl (triple) operator", function() {
+                const input = [
+                    "inc a",
+                    "tpl a",
+                ].join("\n");
+                const instructions = parse(input);
+                const results = execute(instructions);
+
+                expect(results).to.eql({a: 3, b: 0});
+            });
+
+            it("should support the jmp (jump) operator", function() {
+                const input = [
+                    "inc a",
+                    "jmp 3",
+                    "inc a",
+                    "inc b",
+                    "inc a"
+                ].join("\n");
+                const instructions = parse(input);
+                const results = execute(instructions);
+
+                expect(results).to.eql({a: 2, b: 0});
+            });
+
+            it("should support the jie (jump if even) operator", function() {
+                const input = [
+                    "jie a, +2",
+                    "inc a",
+                    "inc b",
+                    "inc a",
+                    "jie a, +2",
+                    "inc a"
+                ].join("\n");
+                const instructions = parse(input);
+                const results = execute(instructions);
+
+                expect(results).to.eql({a: 2, b: 1});
+            });
+
+            it("should support the jio (jump if one) operator", function() {
+                const input = [
+                    "inc b",
+                    "inc a",
+                    "jio a, -2",
+                    "inc b",
+                    "inc a",
+                    "jio a, -2"
+                ].join("\n");
+                const instructions = parse(input);
+                const results = execute(instructions);
+
+                expect(results).to.eql({a: 4, b: 4});
+            });
+            
+            it("should return {a: 2, b: 0}, given the example instruction set", function() {
+                const input = [
+                    "inc a",
+                    "jio a, +2",
+                    "tpl a",
+                    "inc a"
+                ].join("\n");
+                const instructions = parse(input);
+                const results = execute(instructions);
+
+                expect(results).to.eql({a: 2, b: 0});
+            });
+            
+        });
+        
     });
 
 });
