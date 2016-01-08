84f43cbea893c713afda67a49cdb0bb95b0da5b1
Lewis Barnes <lb.barnes@live.co.uk>
2016-01-08 23:38:43 +0000

    First stab at getConfigurations so far. - Currently returns an array of arrays, each containing combinations that add up to the same value. - Added a private function uniquePermutations for the next step. We want to get unique permutations of the combinations currently outputted by getConfigurations.

--- src/day-24/index.js
+++ src/day-24/index.js
@@ -2,6 +2,7 @@ import FS from "fs";
 import Path from "path";
 import _ from "lodash-fp";
 import { split } from "../helpers";
+import { getCombinations } from "../day-17";
 
 export const title = "Day 24: It Hangs in the Balance";
 
@@ -9,6 +10,28 @@ export const parse = _.compose(
     _.map(_.parseInt(10)),
     split("\n")
 );
+
+export function getConfigurations(weights) {
+    return _.compose(
+        // First check there are at least 3 groups.
+        _.filter((x) => x.length >= 3),
+        // Go through each value in weights.
+        // Call getCombinations with 'weights' and the current weight value.
+        _.map((weight) => getCombinations(weights, weight))
+    )(weights);
+};
+
+function uniquePermutations(permutations) {
+    return _.reduce((result, permutation) => {
+        // Check if we already have the permutation.
+        // For exampple, we class [1, 2, 3] and [3, 1, 2] as identical.
+        if (!_.some((x) => !_.difference(x, permutation).length, result))
+            result.push(permutation);
+
+        return result;
+    }, [], permutations);
+};
+
 
 export function run() {
     const inputPath = Path.join(__dirname, "input.txt");
