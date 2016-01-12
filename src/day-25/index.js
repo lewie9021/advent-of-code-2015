14b6df9472865a1c302e5b02781f77a2ef61c15c
Lewis Barnes <lb.barnes@live.co.uk>
2016-01-12 21:16:49 +0000

    Completed the implementation of parse.

--- src/day-25/index.js
+++ src/day-25/index.js
@@ -1,8 +1,16 @@
 import FS from "fs";
 import Path from "path";
 import _ from "lodash-fp";
+import { split, match } from "../helpers";
 
 export const title = "Day 25: Let It Snow";
+
+export const parse = _.compose(
+    _.map(_.parseInt(10)),
+    _.map(_.first),
+    _.map(match(/\d+$/)),
+    split("\n")
+);
 
 export function run() {
     const inputPath = Path.join(__dirname, "input.txt");
